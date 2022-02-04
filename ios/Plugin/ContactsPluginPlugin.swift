3ac7c676e4c12176d02285386c4a29a405453f37
umarmemon1910 <umarmemon1910@gmail.com>
2022-02-04 14:25:46 +0530

    contact plugin

--- ios/Plugin/ContactsPluginPlugin.swift
+++ ios/Plugin/ContactsPluginPlugin.swift
@@ -1,18 +1,58 @@
 import Foundation
 import Capacitor
-
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
-@objc(ContactsPluginPlugin)
-public class ContactsPluginPlugin: CAPPlugin {
-    private let implementation = ContactsPlugin()
-
+import Contacts
+ 
+@objc(ContactsPlugin)
+public class ContactsPlugin: CAPPlugin {
+    
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
+        call.success([
+            "value": value
         ])
+    }
+ 
+    @objc func getContacts(_ call: CAPPluginCall) {
+        let value = call.getString("filter") ?? ""
+        // You could filter based on the value passed to the function!
+        
+        let contactStore = CNContactStore()
+        var contacts = [Any]()
+        let keys = [
+                CNContactFormatter.descriptorForRequiredKeys(for: .fullName),
+                        CNContactPhoneNumbersKey,
+                        CNContactEmailAddressesKey
+                ] as [Any]
+        let request = CNContactFetchRequest(keysToFetch: keys as! [CNKeyDescriptor])
+        
+        contactStore.requestAccess(for: .contacts) { (granted, error) in
+            if let error = error {
+                print("failed to request access", error)
+                call.reject("access denied")
+                return
+            }
+            if granted {
+               do {
+                   try contactStore.enumerateContacts(with: request){
+                           (contact, stop) in
+                    contacts.append([
+                        "firstName": contact.givenName,
+                        "lastName": contact.familyName,
+                        "telephone": contact.phoneNumbers.first?.value.stringValue ?? ""
+                    ])
+                   }
+                   print(contacts)
+                   call.success([
+                       "results": contacts
+                   ])
+               } catch {
+                   print("unable to fetch contacts")
+                   call.reject("Unable to fetch contacts")
+               }
+            } else {
+                print("access denied")
+                call.reject("access denied")
+            }
+        }
     }
 }
--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,8 @@
 export interface ContactsPluginPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
 }
+
+export interface ContactsPluginPlugin {
+  echo(options: { value: string }): Promise<{value: string}>;
+  getContacts(filter: string): Promise<{results: any[]}>;
+}
